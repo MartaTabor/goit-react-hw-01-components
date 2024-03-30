73caf76aa8bc06a0b453cc36bb73b5c16a28e709
Marta Tabor <leeloothefifth@googlemail.com>
2024-03-30 20:53:01 +0000

    statistics amendment plus random color

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,27 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const Statistics = () => {
+export const Statistics = ({ title, stats }) => {
+  const generateRandomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
   return (
     <section className="statistics">
-      <h2 className="title">Upload stats</h2>
-
+      {title && <h2 className="title">{title}</h2>}
       <ul className="stat-list">
-        <li className="item">
-          <span className="label">.docx</span>
-          <span className="percentage">4%</span>
-        </li>
-        <li className="item">
-          <span className="label">.mp3</span>
-          <span className="percentage">14%</span>
-        </li>
-        <li className="item">
-          <span className="label">.pdf</span>
-          <span className="percentage">41%</span>
-        </li>
-        <li className="item">
-          <span className="label">.mp4</span>
-          <span className="percentage">12%</span>
-        </li>
+        {stats.map(stat => (
+          <li
+            className="item"
+            key={stat.id}
+            style={{ backgroundColor: generateRandomColor() }}
+          >
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
       </ul>
     </section>
   );
